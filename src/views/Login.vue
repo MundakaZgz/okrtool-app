9704a114e3c0b7fbf4e19b2d2971e1e55ce286f9
MundakaZgz <j.herrero.moriana@gmail.com>
2022-01-10 23:50:00 +0100

    Add login view

---
+++ src/views/Login.vue
@@ -0,0 +1,54 @@
+// login view
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="email" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" class="form-control">
+      </div>
+      <button class="btn btn-primary">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      axios.post('/login', {
+        email: this.email,
+        password: this.password
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -52,3 +52,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.register {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
